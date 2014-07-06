a1a30965ab3d4f5adfc3645f764ed02d73c9a52d
carlosvin <carlosvin@gmail.com>
2014-07-06 23:52:43 +0200

    adding database

--- web/visits.dart
+++ web/visits.dart
@@ -1,45 +1,162 @@
 
 import 'dart:html';
+import 'dart:indexed_db';
+import 'dart:async';
 
 void main() {
-  new VisitView(new Visit()).form();
+  if (IdbFactory.supported){
+    DB db = new DB();
+    db.open();
+    new VisitFormView(db);
+  }else{
+    var p = querySelector("#main_msg");
+      p..text = 'Necesitas un navegador más moderno'
+       ..classes.add('error'); 
+  }
 }
 
-class VisitView{
+class DB {
+  static const String VISITS_STORE = 'visitsStore';
+  static const String VISITS_DB = 'visitsDB';
+  static const String NAME_INDEX = 'name_index';
   
-  Visit visit;
+  var _db;
+  List<Visit> visits = new List();
   
-  VisitView(Visit visit){
-    this.visit = visit;
+  Future open() {
+    return window.indexedDB.open(VISITS_DB,
+        version: 1,
+        onUpgradeNeeded: _initializeDatabase)
+      .then(_loadFromDB);
+  }
+
+  void _initializeDatabase(VersionChangeEvent e) {
+    Database db = (e.target as Request).result;
+    
+    var objectStore = db.createObjectStore(VISITS_STORE,
+        autoIncrement: true);
+    var index = objectStore.createIndex(NAME_INDEX, 'milestoneName',
+        unique: true);
   }
   
-  
-  void form(){
-    DateInputElement dateInput = new DateInputElement();
-    NumberInputElement boysInput = new NumberInputElement();
-    NumberInputElement girslInput = new NumberInputElement();
-    NumberInputElement menInput = new NumberInputElement();
-    NumberInputElement womenInput = new NumberInputElement();
-    
-    dateInput.valueAsDate = visit.date;
-    boysInput.valueAsNumber = visit.boys;
-    girslInput.valueAsNumber = visit.girls;
-    menInput.valueAsNumber = visit.men;
-    womenInput.valueAsNumber = visit.women;
+  Future _loadFromDB(Database db) {
+    _db = db;
 
-    Element form = querySelector("#input_visits");
+    var trans = db.transaction(VISITS_STORE, 'readonly');
+    var store = trans.objectStore(VISITS_STORE);
+
+    var cursors = store.openCursor(autoAdvance: true).asBroadcastStream();
+    cursors.listen((cursor) {
+      var visit = new Visit.fromRaw(cursor.key, cursor.value);
+      visits.add(visit);
+    });
+
+    return cursors.length.then((_) {
+      return visits.length;
+    });
+  }
+  
+ 
+  Future<Visit> add(Visit visit) {
+     var visitAsMap = visit.toRaw();
+
+     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
+     var objectStore = transaction.objectStore(VISITS_STORE);
+     
+     objectStore.add(visitAsMap).then((addedKey) {
+       // NOTE! The key cannot be used until the transaction completes.
+       visit.id = addedKey;
+     });
+     
+     // Note that the visit cannot be queried until the transaction
+     // has completed!
+     return transaction.completed.then((_) {
+       // Once the transaction completes, add it to our list of available items.
+       visits.add(visit);
+       
+       // Return the milestone so this becomes the result of the future.
+       return visit;
+     });
+   }
+   
+   // Removes a milestone from the list of milestones.
+   // 
+   // This returns a Future which completes when the milestone has been removed.
+   Future remove(Visit visit) {
+     // Remove from database.
+     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
+     transaction.objectStore(VISITS_STORE).delete(visit.id);
+     
+     return transaction.completed.then((_) {
+       // Null out the key to indicate that the milestone is dead.
+       visit.id = null;
+       // Remove from internal list.
+       visits.remove(visit);
+     });
+   }
+   
+   // Removes ALL milestones.
+   Future clear() {
+     // Clear database.
+     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
+     transaction.objectStore(VISITS_STORE).clear();
+     
+     return transaction.completed.then((_) {
+       // Clear internal list.
+       visits.clear();
+     });
+   }
+}
+
+class VisitFormView{
+  
+  Visit visit;
+  DB db;
+  DateInputElement dateInput;
+  NumberInputElement boysInput, girslInput, menInput, womenInput;
+  ButtonElement button;
+  Element form;
+  
+  VisitFormView(DB db){
+    this.db = db;
+    visit = new Visit();
+    dateInput = new DateInputElement();
+    boysInput = new NumberInputElement();
+    girslInput = new NumberInputElement();
+    menInput = new NumberInputElement();
+    womenInput = new NumberInputElement();
+    button = new ButtonElement();
+    button..id = 'save'
+          ..text = 'Salvar'
+          ..classes.add('important')
+          ..onClick.listen((e) => save());
+    
+    form = querySelector("#input_visits");
     form.append(dateInput);
     form.append(boysInput);
     form.append(girslInput);
     form.append(menInput);
     form.append(womenInput);
+    form.append(button);
   }
   
-  void draw(){
+  void loadFormInfo(){
+    visit.boys = boysInput.valueAsNumber;
+    visit.girls = girslInput.valueAsNumber;
+    visit.men = menInput.valueAsNumber;
+    visit.women = womenInput.valueAsNumber;
+    visit.date = dateInput.valueAsDate;
+  }
+  
+  void save(){
+    loadFormInfo();
+    db.add(visit);
   }
 }
 
 class Visit{
+
+  var id;
   DateTime date;
   int boys, girls, men, women;
   
@@ -49,5 +166,25 @@ class Visit{
     girls = 0;
     men = 0;
     women = 0;
-  }   
+  }
+  
+  Visit.fromRaw(key, Map value):
+    id = key,
+    date = value['date'],
+    boys = value['boys'],
+    girls = value['girls'],
+    men = value['men'],
+    women = value['women'];
+  
+  
+  Map toRaw() {
+    return {
+      'date': date,
+      'boys': boys,
+      'girls': girls,
+      'men': men,
+      'women': women,
+    };
+  }
+  
 }
