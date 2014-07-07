aa35029a4cc46038b3c48037529560ccd3f5b25e
carlosvin <carlosvin@gmail.com>
2014-07-07 18:12:14 +0200

    can add elements

--- web/visits.dart
+++ web/visits.dart
@@ -1,18 +1,16 @@
 
 import 'dart:html';
-import 'dart:indexed_db';
 import 'dart:async';
+import 'visits_db.dart';
 
 void main() {
-  if (IdbFactory.supported){
+
+  if (DB.isSupported()){
     DB db = new DB();
-    db.open();
+    db.open().whenComplete(drawList(db));
     new VisitFormView(db);
     
-    TableElement table = querySelector("#visits_table");
-    for (Visit v in db.visits){
-      new VisitRowView(v, table.addRow());
-    }
+    
   }else{
     var p = querySelector("#main_msg");
       p..text = 'Necesitas un navegador más moderno'
@@ -20,98 +18,13 @@ void main() {
   }
 }
 
-class DB {
-  static const String VISITS_STORE = 'visitsStore';
-  static const String VISITS_DB = 'visitsDB';
-  static const String NAME_INDEX = 'name_index';
-  
-  var _db;
-  List<Visit> visits = new List();
-  
-  Future open() {
-    return window.indexedDB.open(VISITS_DB,
-        version: 1,
-        onUpgradeNeeded: _initializeDatabase)
-      .then(_loadFromDB);
-  }
-
-  void _initializeDatabase(VersionChangeEvent e) {
-    Database db = (e.target as Request).result;
-    
-    var objectStore = db.createObjectStore(VISITS_STORE,
-        autoIncrement: true);
-    var index = objectStore.createIndex(NAME_INDEX, 'milestoneName',
-        unique: true);
-  }
-  
-  Future _loadFromDB(Database db) {
-    _db = db;
-
-    var trans = db.transaction(VISITS_STORE, 'readonly');
-    var store = trans.objectStore(VISITS_STORE);
-
-    var cursors = store.openCursor(autoAdvance: true).asBroadcastStream();
-    cursors.listen((cursor) {
-      var visit = new Visit.fromRaw(cursor.key, cursor.value);
-      visits.add(visit);
-    });
-
-    return cursors.length.then((_) {
-      return visits.length;
-    });
-  }
-  
- 
-  Future<Visit> add(Visit visit) {
-     var visitAsMap = visit.toRaw();
-
-     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
-     var objectStore = transaction.objectStore(VISITS_STORE);
-     
-     objectStore.add(visitAsMap).then((addedKey) {
-       // NOTE! The key cannot be used until the transaction completes.
-       visit.id = addedKey;
-     });
-     
-     // Note that the visit cannot be queried until the transaction
-     // has completed!
-     return transaction.completed.then((_) {
-       // Once the transaction completes, add it to our list of available items.
-       visits.add(visit);
-       
-       // Return the milestone so this becomes the result of the future.
-       return visit;
-     });
-   }
-   
-   // Removes a milestone from the list of milestones.
-   // 
-   // This returns a Future which completes when the milestone has been removed.
-   Future remove(Visit visit) {
-     // Remove from database.
-     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
-     transaction.objectStore(VISITS_STORE).delete(visit.id);
-     
-     return transaction.completed.then((_) {
-       // Null out the key to indicate that the milestone is dead.
-       visit.id = null;
-       // Remove from internal list.
-       visits.remove(visit);
-     });
-   }
-   
-   // Removes ALL milestones.
-   Future clear() {
-     // Clear database.
-     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
-     transaction.objectStore(VISITS_STORE).clear();
-     
-     return transaction.completed.then((_) {
-       // Clear internal list.
-       visits.clear();
-     });
-   }
+drawList (DB db){
+  TableElement table = querySelector("#visits_table");
+     for (Visit v in db.visits){
+       new VisitRowView(v, table.addRow());
+     }
 }
+
 
 class VisitFormView{
   
@@ -143,9 +56,11 @@ class VisitFormView{
     form.append(menInput);
     form.append(womenInput);
     form.append(button);
+    
+    dumpToForm();
   }
   
-  void loadFormInfo(){
+  void loadFromForm(){
     visit.boys = boysInput.valueAsNumber;
     visit.girls = girslInput.valueAsNumber;
     visit.men = menInput.valueAsNumber;
@@ -153,20 +68,27 @@ class VisitFormView{
     visit.date = dateInput.valueAsDate;
   }
   
+  void dumpToForm(){
+    boysInput.valueAsNumber = visit.boys;
+    girslInput.valueAsNumber = visit.girls;
+    womenInput.valueAsNumber = visit.women;
+    menInput.valueAsNumber = visit.men;
+    dateInput.valueAsDate = visit.date;
+  }
+  
   void save(){
-    loadFormInfo();
-    Future<Visit> future = db.add(visit);
-    future.whenComplete(saved);
+    loadFromForm();
+    db.add(visit).whenComplete(saved);
   }
   
   void saved(){
-    window.alert("Saved " + visit.id);
+    window.alert("Saved " + visit.getKey());
   }
 }
 
 class Visit{
 
-  var id;
+  //var id;
   DateTime date;
   var boys, girls, men, women;
   
@@ -178,9 +100,8 @@ class Visit{
     women = 0;
   }
   
-  Visit.fromRaw(key, Map value):
-    id = key,
-    date = value['date'],
+  Visit.fromRaw(Map value):
+    date = DateTime.parse(value['date']),
     boys = value['boys'],
     girls = value['girls'],
     men = value['men'],
@@ -189,12 +110,16 @@ class Visit{
   
   Map toRaw() {
     return {
-      'date': date,
+      'date': date.toString(),
       'boys': boys,
       'girls': girls,
       'men': men,
       'women': women,
     };
+  }
+  
+  String getKey(){
+    return date.toString();
   }
 }
 
---
+++ web/visits_db.dart
@@ -0,0 +1,109 @@
+import 'dart:html';
+import 'dart:async';
+import 'visits.dart';
+import 'dart:indexed_db';
+
+class DB {
+  static const String VISITS_STORE = 'visitsStore';
+  static const String VISITS_DB = 'visitsDB';
+  static const String DATE_INDEX = 'date_index';
+  
+  var _db;
+  List<Visit> visits = new List();
+  
+  Future open() {
+    return window.indexedDB.open(VISITS_DB,
+        version: 1,
+        onUpgradeNeeded: _initializeDatabase)
+      .then(_loadFromDB);
+      //.catchError(_onError);
+  }
+
+  void _initializeDatabase(VersionChangeEvent e) {
+    Database db = (e.target as Request).result;
+    
+    var objectStore = db.createObjectStore(VISITS_STORE,
+        autoIncrement: true);
+    var index = objectStore.createIndex(DATE_INDEX, 'date',
+        unique: true);
+  }
+  
+ /* void _onError(e) {
+    window.alert('Oh no! Something went wrong. See the console for details.');
+    window.console.log('An error occurred: {$e}');
+  }*/
+  
+  Future _loadFromDB(Database db) {
+    _db = db;
+
+    var trans = db.transaction(VISITS_STORE, 'readonly');
+    var store = trans.objectStore(VISITS_STORE);
+
+    var cursors = store.openCursor(autoAdvance: true).asBroadcastStream();
+    cursors.listen((cursor) {
+      var visit = new Visit.fromRaw(cursor.value);
+      visits.add(visit);
+    });
+
+    return cursors.length.then((_) {
+      return visits.length;
+    });
+  }
+  
+  Future<Visit> add(Visit visit) {
+     var visitAsMap = visit.toRaw();
+
+     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
+     var objectStore = transaction.objectStore(VISITS_STORE);
+     
+     objectStore.add(visitAsMap, visit.getKey()).then((addedKey) {
+       if (visit.getKey() == addedKey){
+        print("ok: " +  addedKey);
+       }else{
+         print("err: " + addedKey + " != " + visit.getKey());
+       }
+     });
+     
+     // Note that the visit cannot be queried until the transaction
+     // has completed!
+     return transaction.completed.then((_) {
+       // Once the transaction completes, add it to our list of available items.
+       visits.add(visit);
+       
+       // Return the visit so this becomes the result of the future.
+       return visit;
+     });
+   }
+   
+   // Removes a visit from the list of visits.
+   // 
+   // This returns a Future which completes when the visit has been removed.
+   Future remove(Visit visit) {
+     // Remove from database.
+     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
+     transaction.objectStore(VISITS_STORE).delete(visit.getKey());
+     
+     return transaction.completed.then((_) {
+       // Remove from internal list.
+       visits.remove(visit);
+       // Null out the key to indicate that the visit is dead.
+       visit.date = null;
+     });
+   }
+   
+   // Removes ALL visits.
+   Future clear() {
+     // Clear database.
+     var transaction = _db.transaction(VISITS_STORE, 'readwrite');
+     transaction.objectStore(VISITS_STORE).clear();
+     
+     return transaction.completed.then((_) {
+       // Clear internal list.
+       visits.clear();
+     });
+   }
+   
+   static bool isSupported(){
+     return IdbFactory.supported;
+   }
+}
